0154bcb9578ba7cc3fd59b890a7c2fe77295a63a
kevinleemiguel <iradukundaleekevin@gmail.com>
2017-12-20 18:26:16 +0200

    adding json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app                   = express();
 app.set("view engine","ejs");
 
 app.get("/",function(req,res){
-    res.redirect("/index"); 
+    res.render("index"); 
  });
 
  app.listen(process.env.PORT,process.env.IP, function(){
